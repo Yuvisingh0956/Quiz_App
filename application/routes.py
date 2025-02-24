7d6adc2e9bb8dbd0ea69d594246859e1c64a769b
22f3002815 <22f3002815@ds.study.iitm.ac.in>
2025-02-24 22:28:35 +0530

    Further updated

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,21 @@
-from flask import current_app as app
-from flask_security import auth_required
+from flask import current_app as app, jsonify
+from flask_security import auth_required, roles_required, current_user
 
 @app.route('/admin')
-@auth_required('token')
+@auth_required('token') #Authentication
+@roles_required('admin') # Authorization
 def admin_home():
-    return "<h1>This is admin</h1>"
+    return jsonify({
+        'message': 'Welcome to the admin dashboard'
+    })
+
+@app.route('/user')
+@auth_required('token') #Authentication
+@roles_required('user') # Authorization
+def user_home():
+    user = current_user
+    return jsonify({
+        "username": user.username,
+        "email": user.email,
+        "password": user.password
+    })
