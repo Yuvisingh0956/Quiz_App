b136891fbc00b64f823648de60f14e68f31510bf
Yuvraj <58335595+Yuvisingh0956@users.noreply.github.com>
2025-03-30 17:44:31 +0530

    Delete celery_config.py

--- celery_config.py
+++
@@ -1,5 +0,0 @@
-broker_url  = "redis://localhost:6379/0"
-result_backend  = "redis://localhost:6379/1"
-Timezone = "Asia/Kolkata"
-enable_utc = False  # Ensure Celery does not use UTC
-broker_connection_retry_on_startup = True
