7d0ad198aafb8629574f5dea1dd2fd57125e9dd0
Yuvraj <58335595+Yuvisingh0956@users.noreply.github.com>
2025-03-30 17:43:10 +0530

    Delete application directory

--- application/extensions.py
+++
@@ -1,5 +0,0 @@
-# application/extensions.py
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-
-limiter = Limiter(get_remote_address, storage_uri="redis://localhost:6379")
